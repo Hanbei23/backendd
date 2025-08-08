2ecc13fd8d92bfe428e128fc3368de89ca3c9111
Hanbei23 <panduarga49@gmail.com>
2025-08-08 14:53:28 +0700

    fixed

--- app.js
+++ app.js
@@ -8,26 +8,12 @@ import videoRouter from "./routes/video.routes.js";
 
 const app = express();
 
-// Gunakan dynamic CORS
-const allowedOrigins = ["http://localhost:5173", "https://c-tracking.com"];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
+    origin: "http://localhost:5173",
   })
 );
-
-// Handle OPTIONS (preflight) agar CORS sukses
-app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- database/mongodb.js
+++ database/mongodb.js
@@ -1,23 +1,19 @@
 import mongoose from "mongoose";
-
+import { DB_URI, NODE_ENV } from "../config/env.js";
 mongoose.set("strictQuery", false);
 
-const DB_URI = process.env.DB_URI;
-const NODE_ENV = process.env.NODE_ENV || "development";
-
 if (!DB_URI) {
-  throw new Error("Please define the DB_URI environment variable");
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(DB_URI);
     console.log(`MongoDB connected: ${NODE_ENV}`);
   } catch (error) {
-    console.error(`MongoDB connection error: ${error.message}`);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
