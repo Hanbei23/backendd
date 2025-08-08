0ebe7f2b09cf6cd9c591dddd4580ce7b7c020c7b
Hanbei23 <panduarga49@gmail.com>
2025-08-08 14:38:41 +0700

    fix

--- config/env.js
+++ config/env.js
@@ -15,6 +15,6 @@ if (fs.existsSync(envFilePath)) {
 export const PORT = process.env.PORT;
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const SERVER_URL = process.env.SERVER_URL;
-export const DB_URI = process.env.DB_URI || process.env.MONGODB_URI; // fallback
+export const DB_URI = process.env.DB_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
