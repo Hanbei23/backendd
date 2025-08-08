f423c4f28299951ec1e676c4cbccbb81a2f59920
Hanbei23 <panduarga49@gmail.com>
2025-08-08 15:19:47 +0700

    fix app

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/login", authRouter);
+app.use("/api/auth", authRouter);
 app.use("/api/antrian", antrianRouter);
 app.use("/api/link", videoRouter);
 
